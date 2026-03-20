f8d7de666d0ca64c9752dd5e875c835252aaa7f7
TzSam56 <lozanogonzalezcarlossaul@gmail.com>
2026-03-20 10:01:39 -0500

    Add files via upload

---
+++ PracticaDeInterfaz2/src/practicadeinterfaz2/PracticaDeInterfaz2.java
@@ -0,0 +1,17 @@
+//Carlos Saul Lozano Gonzalez
+package practicadeinterfaz2;
+
+/**
+ *
+ * @author lozan
+ */
+public class PracticaDeInterfaz2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
